33a8d2742da92d3201e9b470fcb868b2a8ead2c3
reemhisham32 <96746151+reemhisham32@users.noreply.github.com>
2025-04-22 14:35:50 +0200

    Add files via upload
    Update hello.sh script

---
+++ .github/workflows/hello.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+$ $ #!/bin/bash
+echo "Welcome to my first DevOps script!"
+date
+
+#!/bin/bash
+
+$ 
+
+$ echo "Welcome to my first DevOps script!"
+
+Welcome to my first DevOps script!
+$ 
+
+$ date
+
+Tue Apr 22 17:43:12 IST 2025
+$ 
+
+
